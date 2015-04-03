c681755da23b58e42f22651b13603da900054f95
JohannesTaelman <johannes.taelman@gmail.com>
2015-04-03 14:27:35 +0200

    fw: usbh: less debug output

--- firmware/usbh_conf.h
+++ firmware/usbh_conf.h
@@ -62,7 +62,7 @@
 #define USBH_MAX_NUM_SUPPORTED_CLASS          3
 #define USBH_MAX_SIZE_CONFIGURATION           0x200
 #define USBH_MAX_DATA_BUFFER                  0x200
-#define USBH_DEBUG_LEVEL                      3
+#define USBH_DEBUG_LEVEL                      2
 #define USBH_USE_OS                           1
 
 /** @defgroup USBH_Exported_Macros
