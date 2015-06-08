6dec69e4c1dbd625fef7530848e785fb8f2903f9
JohannesTaelman <johannes.taelman@gmail.com>
2015-06-08 10:57:34 +0200

    dfu flash: leave DFU automatically

--- platform_linux/upload_fw_dfu.sh
+++ platform_linux/upload_fw_dfu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 platformdir="$(dirname $(readlink -f $0))"
 cd $platformdir/../
-dfu-util -v --device 0483:df11 -i 0 -a 0 -D firmware/build/axoloti.bin --dfuse-address=0x08000000
+dfu-util -v --device 0483:df11 -i 0 -a 0 -D firmware/build/axoloti.bin --dfuse-address=0x08000000:leave
