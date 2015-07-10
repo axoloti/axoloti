46761c9a25b30b8b74721bf1a4851dd5786ef060
Mark Harris <mharris@technobear.com>
2015-07-10 12:33:04 +0200

    add shortcut label, to NEW object (from Add)

--- src/main/java/axoloti/PatchFrame.java
+++ src/main/java/axoloti/PatchFrame.java
@@ -431,7 +431,8 @@ jMenuItemSelectAll.addActionListener(new java.awt.event.ActionListener() {
     });
     jMenuEdit.add(jMenuItemSelectAll);
 
-    jMenuItemAddObj.setText("Add Object...");
+    jMenuItemAddObj.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_N, 0));
+    jMenuItemAddObj.setText("New Object...");
     jMenuItemAddObj.addActionListener(new java.awt.event.ActionListener() {
         public void actionPerformed(java.awt.event.ActionEvent evt) {
             jMenuItemAddObjActionPerformed(evt);
@@ -598,13 +599,13 @@ jMenuUploadCode.addActionListener(new java.awt.event.ActionListener() {
     jMenuWindow.setMnemonic('W');
     jMenuWindow.setText("Window");
     jMenuWindow.addMenuListener(new javax.swing.event.MenuListener() {
-        public void menuCanceled(javax.swing.event.MenuEvent evt) {
+        public void menuSelected(javax.swing.event.MenuEvent evt) {
+            jMenuWindowMenuSelected(evt);
         }
         public void menuDeselected(javax.swing.event.MenuEvent evt) {
             jMenuWindowMenuDeselected(evt);
         }
-        public void menuSelected(javax.swing.event.MenuEvent evt) {
-            jMenuWindowMenuSelected(evt);
+        public void menuCanceled(javax.swing.event.MenuEvent evt) {
         }
     });
     jMenuBar1.add(jMenuWindow);
@@ -911,7 +912,7 @@ jMenuUploadCode.addActionListener(new java.awt.event.ActionListener() {
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void jMenuItemAddObjActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemAddObjActionPerformed
-        patch.ShowClassSelector(new Point(20, 20), null);
+        patch.ShowClassSelector(new Point(20, 20), null,null);
     }//GEN-LAST:event_jMenuItemAddObjActionPerformed
 
     private void jMenuCloseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuCloseActionPerformed
