5758a43c838989cf332a6d9576cf19d06a395939
Mark Harris <mharris@technobear.com>
2015-07-13 23:27:40 +0200

    install: move chibios/cmsis/patch to release package

--- platform_osx/compile_patch.sh
+++ platform_osx/compile_patch.sh
@@ -7,5 +7,5 @@ export axoloti_firmware=${axoloti_firmware:="$axoloti_release/firmware"}
 export axoloti_home=${axoloti_home:="$rootdir"}
 
 export PATH=$PATH:${platformdir}/gcc-arm-none-eabi-4_8-2014q3/bin/:${platformdir}/bin
-cd ${axoloti_runtime}/patch
+cd ${axoloti_release}/patch
 make
