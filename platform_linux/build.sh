a969a4451cb9363c5984ce5fcae397c8eada6b53
JohannesTaelman <johannes.taelman@gmail.com>
2015-07-27 22:36:35 +0200

    linux build: non-interactive

--- platform_linux/build.sh
+++ platform_linux/build.sh
@@ -8,8 +8,8 @@ echo -e "Use at your own risk\n"
 echo -e "Some packages will be installed with apt-get,"
 echo -e "and all users will be granted permission to access some USB devices"
 echo -e "For this you'll require sudo rights and need to enter your password...\n"
-echo -e "Press RETURN to continue\nCTRL-C if you are unsure!\n"
-read
+# echo -e "Press RETURN to continue\nCTRL-C if you are unsure!\n"
+# read
 
 
 echo "apt-get install -y libtool libudev-dev automake autoconf ant curl lib32z1 lib32ncurses5 lib32bz2-1.0"
