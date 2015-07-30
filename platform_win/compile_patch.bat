dfd73e79a8255e3e2dc414492229dc957aa6da0d
JohannesTaelman <johannes.taelman@gmail.com>
2015-07-30 12:21:37 +0200

    windows bundle: path shortening

--- platform_win/compile_patch.bat
+++ platform_win/compile_patch.bat
@@ -1,4 +1,8 @@
 @ECHO OFF
-CALL %~sdp0\path.bat
+call %~sdp0\path.bat
+rem echo path %PATH%
+rem echo firmware %axoloti_firmware%
+rem echo runtime %axoloti_runtime%
+rem echo release %axoloti_release%
 cd %axoloti_release%\patch
 make
--- platform_win/path.bat
+++ platform_win/path.bat
@@ -3,13 +3,27 @@ set platformdir=%~sdp0
 if not defined axoloti_release (
    set axoloti_release=%platformdir%..
 )
+
 if not defined axoloti_runtime (
-   set axoloti_runtime=%platformdir%..
+   set axoloti_runtime=%platformdir%\..
 )
+
 if not defined axoloti_firmware (
-   set axoloti_firmware=%axoloti_release%\firmware
+   call :setfirmware "%axoloti_release%\firmware"
 )
+call :setrelease "%axoloti_release%"
+
 if not defined axoloti_home (
    set axoloti_home=%platformdir%..
 )
-set PATH=%platformdir%\bin
+set PATH=%platformdir%bin
+
+goto :eof
+
+:setfirmware
+set axoloti_firmware=%~s1
+goto :eof
+
+:setrelease
+set axoloti_release=%~s1
+goto :eof
