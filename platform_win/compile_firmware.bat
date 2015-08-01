0eaef044df2bf70f20987737ed5700cc61751644
JohannesTaelman <johannes.taelman@gmail.com>
2015-08-01 20:06:26 +0200

    windows compile_firmware fix

--- platform_win/compile_firmware.bat
+++ platform_win/compile_firmware.bat
@@ -1,6 +1,12 @@
 @ECHO OFF
+setlocal
+if not defined axoloti_runtime (
+   set axoloti_runtime=%~sdp0
+)
 call %~sdp0\path.bat
 call %~sdp0\test_env.bat
 
 cd %axoloti_firmware%
-call $axoloti_firmware%\compile_firmware_win.bat
+call %axoloti_firmware%\compile_firmware_win.bat
+
+endlocal
