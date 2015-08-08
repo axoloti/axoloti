eb4cd4e45a12aa5c9d51e69e2cf0c5a23a0712b4
Mark Harris <mharris@technobear.com>
2015-08-08 18:14:49 +0200

    make upload_fw consistent with osx version

--- platform_linux/upload_fw_dfu.sh
+++ platform_linux/upload_fw_dfu.sh
@@ -8,8 +8,8 @@ export axoloti_home=${axoloti_home:="$platformdir"}
 
 if [ -f "${platformdir}/bin/dfu-util" ];
 then
-    cd "${platformdir}"/../
-    "${platformdir}"/bin/dfu-util --device 0483:df11 -i 0 -a 0 -D "${axoloti_firmware}/build/axoloti.bin" --dfuse-address=0x08000000:leave
+    cd "${platformdir}/bin"
+    ./bin/dfu-util --device 0483:df11 -i 0 -a 0 -D "${axoloti_firmware}/build/axoloti.bin" --dfuse-address=0x08000000:leave
 else
     echo "dfu-util not found, run ./install.sh in axoloti/platform_linux"
 fi
