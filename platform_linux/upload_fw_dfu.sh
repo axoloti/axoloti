c29b19ca334c2cab6916982b06e16a7a64ae66ae
Mark Harris <mharris@technobear.com>
2015-08-12 04:58:24 -0700

    incorrect dfu-util path (linux), fixes issue #223

--- platform_linux/upload_fw_dfu.sh
+++ platform_linux/upload_fw_dfu.sh
@@ -9,7 +9,7 @@ export axoloti_home=${axoloti_home:="$platformdir"}
 if [ -f "${platformdir}/bin/dfu-util" ];
 then
     cd "${platformdir}/bin"
-    ./bin/dfu-util --device 0483:df11 -i 0 -a 0 -D "${axoloti_firmware}/build/axoloti.bin" --dfuse-address=0x08000000:leave
+    ./dfu-util --device 0483:df11 -i 0 -a 0 -D "${axoloti_firmware}/build/axoloti.bin" --dfuse-address=0x08000000:leave
 else
     echo "dfu-util not found, run ./install.sh in axoloti/platform_linux"
 fi
