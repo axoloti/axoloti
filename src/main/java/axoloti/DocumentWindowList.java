e179e5ee14395cde97a65878fd81fdb462a908c7
JohannesTaelman <johannes.taelman@gmail.com>
2016-01-21 09:04:38 +0100

    GUI: DocumentWindow/List added

---
+++ src/main/java/axoloti/DocumentWindowList.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2013 - 2016 Johannes Taelman
+ *
+ * This file is part of Axoloti.
+ *
+ * Axoloti is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Axoloti is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Axoloti. If not, see <http://www.gnu.org/licenses/>.
+ */
+package axoloti;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jtaelman
+ */
+public class DocumentWindowList {
+
+    static ArrayList<DocumentWindow> list = new ArrayList<DocumentWindow>();
+
+    static public void RegisterWindow(DocumentWindow w) {
+        list.add(w);
+    }
+
+    static public void UnregisterWindow(DocumentWindow w) {
+        list.remove(w);
+    }
+
+    static public ArrayList<DocumentWindow> GetList() {
+        return list;
+    }
+}
