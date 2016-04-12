fb6ff0a0b194fe1e36a323ae2be5b3f40dae93c5
Mark Harris <mharris@technobear.com>
2016-04-12 11:06:38 +0200

    allow compile firmware (mac/linux) to be passed clean

--- firmware/compile_firmware_linux.sh
+++ firmware/compile_firmware_linux.sh
@@ -7,7 +7,7 @@ make -f Makefile.patch clean
 echo "Compiling firmware... ${axoloti_firmware}"
 mkdir -p build/obj
 mkdir -p build/lst
-if ! make ; then
+if ! make $1 ; then
     exit 1
 fi
 
@@ -15,12 +15,12 @@ echo "Compiling firmware flasher..."
 cd flasher
 mkdir -p flasher_build/lst
 mkdir -p flasher_build/obj
-make
+make $1
 cd ..
 
 echo "Compiling firmware mounter..."
 cd mounter
 mkdir -p mounter_build/lst
 mkdir -p mounter_build/obj
-make
+make $1
 cd ..
