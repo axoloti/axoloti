2cd0938509aa601d4408fef8b45ec7c9bf47bd49
Johannes Taelman <johannes.taelman@gmail.com>
2016-08-16 17:09:17 +0200

    GUI: cleanup receiver thread

--- src/main/java/axoloti/USBBulkConnection.java
+++ src/main/java/axoloti/USBBulkConnection.java
@@ -130,6 +130,15 @@ public class USBBulkConnection extends Connection {
                 sync.notifyAll();
             }
 
+            if (receiverThread.isAlive()){
+                receiverThread.interrupt();
+                try {
+                    receiverThread.join();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(USBBulkConnection.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
             int result = LibUsb.releaseInterface(handle, interfaceNumber);
             if (result != LibUsb.SUCCESS) {
                 throw new LibUsbException("Unable to release interface", result);
