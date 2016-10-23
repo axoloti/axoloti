53da8a2954e2ac4caf7edd737b400f978f8f1768
JohannesTaelman <johannes.taelman@gmail.com>
2016-10-23 21:08:20 +0200

    FW: remove 808 samples, closes #105

--- firmware/samples/objcopy.bat
+++
@@ -1,7 +0,0 @@
-arm-none-eabi-objcopy -I binary -O elf32-little -B arm --rename-section .data=.samples,alloc,load,readonly,contents 808bd.raw 808bd.o
-arm-none-eabi-objcopy -I binary -O elf32-little -B arm --rename-section .data=.samples,alloc,load,readonly,contents 808hatclose.raw 808hatclose.o
-arm-none-eabi-objcopy -I binary -O elf32-little -B arm --rename-section .data=.samples,alloc,load,readonly,contents 808hatopen.raw 808hatopen.o
-arm-none-eabi-objcopy -I binary -O elf32-little -B arm --rename-section .data=.samples,alloc,load,readonly,contents 808hitom.raw 808hitom.o
-arm-none-eabi-objcopy -I binary -O elf32-little -B arm --rename-section .data=.samples,alloc,load,readonly,contents 808lotom.raw 808lotom.o
-arm-none-eabi-objcopy -I binary -O elf32-little -B arm --rename-section .data=.samples,alloc,load,readonly,contents 808midtom.raw 808midtom.o
-arm-none-eabi-objcopy -I binary -O elf32-little -B arm --rename-section .data=.samples,alloc,load,readonly,contents 808snare.raw 808snare.o
