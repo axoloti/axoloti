f959d81d6dbb90690eaa0a8dffd9bd097dffdcb8
Mark Harris <mharris@technobear.com>
2016-11-14 22:29:16 +0100

    FW:elements experiments

--- firmware/xpatch.h
+++ firmware/xpatch.h
@@ -16,3 +16,7 @@ extern "C" {
 #include "elements/dsp/exciter.h"
 #include "elements/dsp/tube.h"
 #include "elements/dsp/string.h"
+#include "elements/dsp/fx/diffuser.h"
+#include "elements/dsp/fx/reverb.h"
+//#include "elements/dsp/part.h"
+//#include "elements/dsp/patch.h"
