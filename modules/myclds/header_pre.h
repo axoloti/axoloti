eecf8d603c5da012fc255b4ab5b4122698f9084c
Mark Harris <mharris@technobear.com>
2017-03-05 21:23:22 +0100

    modules:experimental working

---
+++ modules/myclds/header_pre.h
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <inttypes.h>
+#include <stddef.h>
+#include <cmath>
+#include <cstdio>
+#include <cctype>
+#include <clocale>
+
+
