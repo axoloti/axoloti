852b502e1555ec1f5078b1611a445651452eb942
Mark Harris <mharris@technobear.com>
2017-03-06 01:59:13 +0100

    modules:warps attempt

---
+++ modules/mywrps/header_pre.h
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <inttypes.h>
+#include <stddef.h>
+#include <cmath>
+#include <cstdio>
+#include <cctype>
+#include <clocale>
+
+#include "axoloti_memory.h"
+#include "stmlib/dsp/filter.h"
+#include "stmlib/dsp/cosine_oscillator.h"
+#include "stmlib/dsp/dsp.h"
+#include "stmlib/dsp/parameter_interpolator.h"
+#include "stmlib/dsp/units.h"
+#include "stmlib/stmlib.h"
+#include "stmlib/utils/random.h"
+
+
---
+++ modules/mywrps/mywrps.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "warps/dsp/modulator.h"
+#include "warps/dsp/vocoder.h"
