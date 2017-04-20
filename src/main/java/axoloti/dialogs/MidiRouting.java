992427603e3dce17529d30ca528db3f6894d1212
Johannes Taelman <johannes.taelman@gmail.com>
2017-04-20 10:12:15 +0200

    GUI: MidiRouting dialog

--- src/main/java/axoloti/dialogs/MidiRouting.java
+++ src/main/java/axoloti/dialogs/MidiRouting.java
@@ -7,8 +7,6 @@ import axoloti.chunks.ChunkData;
 import axoloti.chunks.FourCCs;
 import java.awt.event.ActionEvent;
 import java.nio.ByteBuffer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.AbstractAction;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -16,11 +14,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTree;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
 import qcmds.QCmdMemRead;
 
 /**
@@ -34,32 +29,54 @@ public class MidiRouting extends javax.swing.JFrame implements ConnectionStatusL
         CConnection.GetConnection().addConnectionStatusListener(this);
     }
 
-    JTable table_inputs;
-    JTable table_outputs;
-//    DefaultMutableTreeNode top;
+    JTable table_midi_in_routing;
+    JTable table_midi_out_routing;
 
     void initComponents() {
         setMinimumSize(new java.awt.Dimension(200, 160));
-//        top = new DefaultMutableTreeNode("midi routing");
-        table_inputs = new JTable();
-        table_outputs = new JTable();
-        table_inputs.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        table_outputs.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        table_inputs.setShowGrid(true);
-        table_outputs.setShowGrid(true);
-        JPanel vPane1 = new JPanel();
-        vPane1.setLayout(new BoxLayout(vPane1, BoxLayout.PAGE_AXIS));
-        vPane1.add(new JLabel("MIDI input routing"));
-        vPane1.add(table_inputs);
-        vPane1.add(new JLabel("MIDI output routing"));
-        vPane1.add(table_outputs);
-        vPane1.setAlignmentX(0);
-        vPane1.setAlignmentY(0);
-        JScrollPane scrollpane = new JScrollPane(vPane1);
+        table_midi_in_routing = new JTable(new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return (column > 0);
+            }
+
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                if (columnIndex > 0) {
+                    return Boolean.class;
+                } else {
+                    return String.class;
+                }
+            }
+
+        });
+        table_midi_in_routing.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        table_midi_in_routing.setShowGrid(true);
+        table_midi_out_routing = new JTable(new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return (column > 0);
+            }
+
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                if (columnIndex > 0) {
+                    return Boolean.class;
+                } else {
+                    return String.class;
+                }
+            }
+
+        });
+        table_midi_out_routing.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        table_midi_out_routing.setShowGrid(true);
+        JScrollPane scrollpane_in = new JScrollPane(table_midi_in_routing);
+        JScrollPane scrollpane_out = new JScrollPane(table_midi_out_routing);
         JPanel vPane = new JPanel();
         vPane.setLayout(new BoxLayout(vPane, BoxLayout.PAGE_AXIS));
         vPane.add(new JLabel("work in progress... todo: allow modification, apply changes..."));
-        vPane.add(scrollpane);
+        vPane.add(scrollpane_in);
+        vPane.add(scrollpane_out);
         JPanel bPane = new JPanel();
         bPane.setLayout(new BoxLayout(bPane, BoxLayout.LINE_AXIS));
         JButton buttonRefresh = new JButton("Refresh");
@@ -94,31 +111,18 @@ public class MidiRouting extends javax.swing.JFrame implements ConnectionStatusL
     void apply() {
     }
 
-    void refresh() {
-        DefaultTableModel tm_in = (DefaultTableModel) table_inputs.getModel();
-        tm_in.setRowCount(0);
-        tm_in.setColumnIdentifiers(new Object[]{"input port",
-            "virtual in1",
-            "v 2",
-            "v 3",
-            "v 4",
-            "v 5",
-            "v 6",
-            "v 7",
-            "v 8",
-            "v 9",
-            "v 10",
-            "v 11",
-            "v 12",
-            "v 13",
-            "v 14",
-            "v 15",
-            "v 16"
-        });
-        DefaultTableModel tm_out = (DefaultTableModel) table_outputs.getModel();
-        tm_out.setRowCount(0);
-        tm_out.setColumnIdentifiers(new Object[]{"output port",
-            "virtual out #1",
+    private Object[] CreateEmptyTableRowData() {
+        Object[] rowdata = new Object[17];
+        rowdata[0] = "";
+        for (int i = 1; i < 17; i++) {
+            rowdata[i] = false;
+        }
+        return rowdata;
+    }
+
+    private Object[] CreateColumnIds() {
+        return new Object[]{"port",
+            "#1",
             "#2",
             "#3",
             "#4",
@@ -134,16 +138,22 @@ public class MidiRouting extends javax.swing.JFrame implements ConnectionStatusL
             "#14",
             "#15",
             "#16"
-        });
-        IConnection conn = CConnection.GetConnection();
-        ChunkData[] chunk_inputs = conn.GetFWChunks().GetAll(FourCCs.FW_MIDI_INPUT_ROUTING);
-        ChunkData[] chunk_outputs = conn.GetFWChunks().GetAll(FourCCs.FW_MIDI_OUTPUT_ROUTING);
-//        DefaultMutableTreeNode inputs = new DefaultMutableTreeNode("inputs");
-        for (ChunkData cd : chunk_inputs) {
+        };
+    }
+
+    class CInputRoutingData {
+
+        final int destinations_per_port = 4;
+        final String portname;
+        final int nports;
+        final int table_addr;
+        final int vports[][];
+
+        CInputRoutingData(IConnection conn, ChunkData cd) {
             cd.data.rewind();
             int name_addr = cd.data.getInt();
-            int nports = cd.data.getInt();
-            int table_addr = cd.data.getInt();
+            nports = cd.data.getInt();
+            table_addr = cd.data.getInt();
             conn.ClearReadSync();
             conn.AppendToQueue(new QCmdMemRead(name_addr, 60));
             conn.WaitReadSync();
@@ -153,71 +163,128 @@ public class MidiRouting extends javax.swing.JFrame implements ConnectionStatusL
                 c += (char) b;
                 b = conn.getMemReadBuffer().get();
             }
+            portname = c;
             conn.ClearReadSync();
             conn.AppendToQueue(new QCmdMemRead(table_addr, nports * 4));
             conn.WaitReadSync();
             ByteBuffer routing_table = conn.getMemReadBuffer();
-            System.out.println("input " + c + " table addr " + String.format("0x%08X", table_addr));
+            vports = new int[nports][destinations_per_port];
             for (int i = 0; i < nports; i++) {
-                Object[] rowdata = new Object[17];
-                for (int k = 0; k < 17; k++) {
-                    rowdata[k] = ".";
+                for (int j = 0; j < destinations_per_port; j++) {
+                    vports[i][j] = routing_table.get();
                 }
-                if (nports > 1) {
-                    rowdata[0] = c + " #" + i;
+            }
+            System.out.println("input " + portname + " table addr " + String.format("0x%08X", table_addr));
+        }
+    }
+
+    private CInputRoutingData[] readInputMapping() {
+        IConnection conn = CConnection.GetConnection();
+        ChunkData[] chunk_inputs = conn.GetFWChunks().GetAll(FourCCs.FW_MIDI_INPUT_ROUTING);
+        CInputRoutingData[] cirs = new CInputRoutingData[chunk_inputs.length];
+        for (int i = 0; i < chunk_inputs.length; i++) {
+            cirs[i] = new CInputRoutingData(conn, chunk_inputs[i]);
+        }
+        return cirs;
+    }
+
+    private void refreshInputs(CInputRoutingData[] inputroutingdata) {
+        DefaultTableModel tm_in = (DefaultTableModel) table_midi_in_routing.getModel();
+        tm_in.setRowCount(0);
+        tm_in.setColumnIdentifiers(CreateColumnIds());
+        table_midi_in_routing.setRowSelectionAllowed(false);
+        table_midi_in_routing.setColumnSelectionAllowed(false);
+        for (CInputRoutingData ird : inputroutingdata) {
+            for (int i = 0; i < ird.nports; i++) {
+                Object[] rowdata = new Object[17];
+                for (int k = 1; k < 17; k++) {
+                    rowdata[k] = false;
+                }
+                if (ird.nports > 1) {
+                    rowdata[0] = ird.portname + " #" + i + " ->";
                 } else {
-                    rowdata[0] = c;
+                    rowdata[0] = ird.portname + " ->";
                 }
                 for (int j = 0; j < 4; j++) {
-                    byte dest = routing_table.get();
+                    int dest = ird.vports[i][j];
                     if (dest != -1) {
-                        rowdata[dest + 1] = "x";
+                        rowdata[dest + 1] = true;
                     }
                 }
                 tm_in.addRow(rowdata);
             }
         }
-
-        String[] rowdata = new String[17];
-        rowdata[0] = "DIN";
-        tm_out.addRow(rowdata);
-        rowdata = new String[17];
-        rowdata[0] = "USB Device";
-        tm_out.addRow(rowdata);
-        for(int i=0;i<16;i++){
-            rowdata = new String[17];
-            rowdata[0] = "USB Host #" + i;
-            tm_out.addRow(rowdata);            
+        table_midi_in_routing.getColumnModel().getColumn(0).setPreferredWidth(150);
+        for (int i = 1; i < 17; i++) {
+            table_midi_in_routing.getColumnModel().getColumn(i).setPreferredWidth(25);
         }
-        
-        for (ChunkData cd : chunk_outputs) {
-            cd.data.rewind();
-            int ntargets = cd.data.getInt();
-            int table_addr = cd.data.getInt();
+        table_midi_in_routing.doLayout();
+    }
+
+    final int ntargets = 4;
+
+    private ByteBuffer readOutputMapping() {
+        IConnection conn = CConnection.GetConnection();
+        if (conn.isConnected()) {
+            ChunkData chunk_output = conn.GetFWChunks().GetOne(FourCCs.FW_MIDI_OUTPUT_ROUTING);
+            chunk_output.data.rewind();
+            int ntargets = chunk_output.data.getInt();
+            int table_addr = chunk_output.data.getInt();
             conn.ClearReadSync();
             conn.AppendToQueue(new QCmdMemRead(table_addr, ntargets * 16 * 2));
             conn.WaitReadSync();
             ByteBuffer routing_table = conn.getMemReadBuffer();
-            
-            for (int vport = 0; vport < 16; vport++) {                
-                for (int j = 0; j < ntargets; j++) {
-                    byte target1 = routing_table.get();
-                    byte target2 = routing_table.get();
-                    if (target1 != 0) {
-                        switch (target1) {
-                            case 1: // DIN
-                                tm_out.setValueAt("x", 0, target1);
-                                break;
-                            case 2: // USB Device port
-                                tm_out.setValueAt("x", 1, target1);
-                                break;
-                            case 3: // USB Host port
-                                tm_out.setValueAt("x", 2+target2, target1);
-                                break;
-                        }
+            return routing_table;
+        } else {
+            return ByteBuffer.wrap(new byte[16 * 4 * 2]);
+        }
+    }
+
+    private void refreshOutputs(ByteBuffer output_routing_table_data) {
+        DefaultTableModel tm_out = (DefaultTableModel) table_midi_out_routing.getModel();
+        table_midi_out_routing.setRowSelectionAllowed(false);
+        table_midi_out_routing.setColumnSelectionAllowed(false);
+        tm_out.setRowCount(0);
+        tm_out.setColumnIdentifiers(CreateColumnIds());
+        Object[] rowdata = CreateEmptyTableRowData();
+        rowdata[0] = "DIN <-";
+        tm_out.addRow(rowdata);
+        rowdata = CreateEmptyTableRowData();
+        rowdata[0] = "USB Device <-";
+        tm_out.addRow(rowdata);
+        for (int i = 0; i < 16; i++) {
+            rowdata = CreateEmptyTableRowData();
+            rowdata[0] = "USB Host #" + i + " <-";
+            tm_out.addRow(rowdata);
+        }
+        for (int vport = 0; vport < 16; vport++) {
+            for (int j = 0; j < ntargets; j++) {
+                byte target1 = output_routing_table_data.get();
+                byte target2 = output_routing_table_data.get();
+                if (target1 != 0) {
+                    switch (target1) {
+                        case 1: // DIN
+                            tm_out.setValueAt(true, 0, target1);
+                            break;
+                        case 2: // USB Device port
+                            tm_out.setValueAt(true, 1, target1);
+                            break;
+                        case 3: // USB Host port
+                            tm_out.setValueAt(true, 2 + target2, target1);
+                            break;
                     }
                 }
             }
         }
+        table_midi_out_routing.getColumnModel().getColumn(0).setPreferredWidth(150);
+        for (int i = 1; i < 17; i++) {
+            table_midi_out_routing.getColumnModel().getColumn(i).setPreferredWidth(25);
+        }
+        table_midi_out_routing.doLayout();
+    }
+
+    void refresh() {
+        refreshInputs(readInputMapping());
+        refreshOutputs(readOutputMapping());
     }
 }
