axoloti/axoloti

commit 586f59a88e1481fc18754b96b8f7aeef7894bf33

GUI: potpourri commit
introducing ListProperty
remove AbstractController::getParent()
add IModel::getParent()
add IModel::getControllerFromView()
move modelPropertyChange calls to controller classes
ArrayView improved approach
added tests...

fixed: autoscroll when dragging net
fixed: autoscroll when dragging object
fixed: objectEditor view updating
fixed: undo midi cc mapping
fixed: removing objects with parameter-on-parent

feature: undo/redo edits window
feature: follow view when undoing/redoing edits

and more...

src/main/java/axoloti/patch/object/AxoObjectInstanceFactory.java
@@ -31,7 +31,6 @@ public class AxoObjectInstanceFactory {
         AxoObjectInstanceAbstract obji;
         if (objm instanceof AxoObjectUnloaded) {
             objm = ((AxoObjectUnloaded) objm).Load();
-            objc = objm.createController(null, null);
         }
         if (objm instanceof AxoObjectComment) {
             obji = new AxoObjectInstanceComment(objc, patchModel, instanceName, location);
@@ -40,8 +39,10 @@ public class AxoObjectInstanceFactory {
         } else if (objm instanceof AxoObjectPatcher) {
             // every AxoObjectPatcherInstance needs an independent AxoObjectPatcher object
             AxoObjectPatcher obj = new AxoObjectPatcher();
-            objc = new ObjectController(obj, null);
-            obji = new AxoObjectInstancePatcher(objc, patchModel, instanceName, location);
+            if (patchController != null) {
+                obj.setDocumentRoot(patchController.getDocumentRoot());
+            }
+            obji = new AxoObjectInstancePatcher(obj.getControllerFromModel(), patchModel, instanceName, location);
         } else if (objm instanceof AxoObjectPatcherObject) {
             AxoObjectPatcherObject objm1 = new AxoObjectPatcherObject();
             AbstractDocumentRoot dr;
@@ -50,7 +51,8 @@ public class AxoObjectInstanceFactory {
             } else {
                 dr = null;
             }
-            objc = objm1.createController(dr, patchController);
+            objm1.setDocumentRoot(dr);
+            objc = objm1.getControllerFromModel();
             obji = new AxoObjectInstancePatcherObject(objc, patchModel, instanceName, location);
         } else if (objm instanceof AxoObjectZombie) {
             obji = new AxoObjectInstanceZombie(objc, patchModel, instanceName, location);

commit 33a7d04df448b0185e18522057476ebe8dff99e4

GUI: fix loading objectFormPatch
broken in 586f59a88e1481fc18754b96b8f7aeef7894bf33
wrong fix in ace038951f42d5ac856193b5a23e65a260fc2573

src/main/java/axoloti/patch/object/AxoObjectInstanceFactory.java
@@ -31,6 +31,7 @@ public class AxoObjectInstanceFactory {
         AxoObjectInstanceAbstract obji;
         if (objm instanceof AxoObjectUnloaded) {
             objm = ((AxoObjectUnloaded) objm).Load();
+            objc = objm.getControllerFromModel();
         }
         if (objm instanceof AxoObjectComment) {
             obji = new AxoObjectInstanceComment(objc, patchModel, instanceName, location);