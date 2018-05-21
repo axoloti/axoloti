axoloti/axoloti

commit 1ab460c6d3643587a94963f15f0522fcd9e1b407

GUI: major cleanup
IModel->getControllerFromModel() replaced with IModel->getController()

IVIew->getController() removed, use IView->getDModel()->getController()

methods names starting with upper case -> lower case

cleanup parameter classes

cleanup patch/patcher and patch/object implementation

and more...

src/main/java/axoloti/codegen/patch/object/AxoObjectInstanceCodegenViewFactory.java
@@ -6,7 +6,6 @@ import axoloti.patch.object.AxoObjectInstanceHyperlink;
 import axoloti.patch.object.AxoObjectInstancePatcher;
 import axoloti.patch.object.IAxoObjectInstance;
 import axoloti.patch.object.ObjectInstanceController;
-import axoloti.patch.object.ObjectInstancePatcherController;
 
 /**
  *
@@ -14,17 +13,17 @@ import axoloti.patch.object.ObjectInstancePatcherController;
  */
 public class AxoObjectInstanceCodegenViewFactory {
 
-    public static IAxoObjectInstanceCodegenView createView(ObjectInstanceController controller) {
-        IAxoObjectInstance model = controller.getModel();
+    public static IAxoObjectInstanceCodegenView createView(IAxoObjectInstance model) {
+        ObjectInstanceController controller = model.getController();
         IAxoObjectInstanceCodegenView view;
         if (model instanceof AxoObjectInstancePatcher) {
-            view = new AxoObjectInstancePatcherCodegenView((ObjectInstancePatcherController) controller);
+            view = new AxoObjectInstancePatcherCodegenView(model);
         } else if (model instanceof AxoObjectInstance) {
-            view = new AxoObjectInstanceCodegenView(controller);
+            view = new AxoObjectInstanceCodegenView(model);
         } else if (model instanceof AxoObjectInstanceComment) {
-            view = new AxoObjectInstanceDummyCodegenView(controller);
+            view = new AxoObjectInstanceDummyCodegenView(model);
         } else if (model instanceof AxoObjectInstanceHyperlink) {
-            view = new AxoObjectInstanceDummyCodegenView(controller);
+            view = new AxoObjectInstanceDummyCodegenView(model);
         } else {
             return null;
         }

src/main/java/axoloti/codegen/patch/object/AxoObjectInstancePatcherCodegenView.java
@@ -2,11 +2,10 @@ package axoloti.codegen.patch.object;
 
 import axoloti.codegen.patch.PatchViewCodegen;
 import axoloti.object.AxoObjectPatcher;
-import axoloti.patch.PatchController;
 import axoloti.patch.PatchModel;
 import axoloti.patch.SubPatchMode;
 import axoloti.patch.object.AxoObjectInstancePatcher;
-import axoloti.patch.object.ObjectInstancePatcherController;
+import axoloti.patch.object.IAxoObjectInstance;
 
 /**
  *
@@ -14,13 +13,12 @@ import axoloti.patch.object.ObjectInstancePatcherController;
  */
 public class AxoObjectInstancePatcherCodegenView extends AxoObjectInstanceCodegenView {
 
-    AxoObjectInstancePatcherCodegenView(ObjectInstancePatcherController controller) {
-        super(controller);
-        AxoObjectInstancePatcher model = controller.getModel();
-        AxoObjectPatcher ao = (AxoObjectPatcher)model.getController().getModel();
-        PatchController pc = controller.getSubPatchController();
-        PatchModel pm = pc.getModel();
-        PatchViewCodegen pvcg = new PatchViewCodegen(pc);
+    AxoObjectInstancePatcherCodegenView(IAxoObjectInstance objectInstance) {
+        super(objectInstance);
+        AxoObjectInstancePatcher model = (AxoObjectInstancePatcher) objectInstance;
+        AxoObjectPatcher ao = (AxoObjectPatcher) model.getDModel();
+        PatchModel pm = model.getSubPatchModel();
+        PatchViewCodegen pvcg = new PatchViewCodegen(pm);
 
         // from GenerateAxoObjNormal
         if (pm.getSubPatchMode() == SubPatchMode.no ||
@@ -66,13 +64,13 @@ public class AxoObjectInstancePatcherCodegenView extends AxoObjectInstanceCodege
                     + "if ( attr_midiport > 0 && port > 0 && attr_midiport != port) return;\n"
                     + pvcg.GenerateMidiInCodePlusPlus();
 */
-            pvcg.GenerateNormalCode(ao);
+            pvcg.generateNormalCode(ao);
         } else if (pm.getSubPatchMode() == SubPatchMode.polyphonic) {
-            pvcg.GeneratePolyCode(ao);
+            pvcg.generatePolyCode(ao);
         } else if (pm.getSubPatchMode() == SubPatchMode.polychannel) {
-            pvcg.GeneratePolyChannelCode(ao);
+            pvcg.generatePolyChannelCode(ao);
         } else if (pm.getSubPatchMode() == SubPatchMode.polyexpression) {
-            pvcg.GeneratePolyExpressionCode(ao);
+            pvcg.generatePolyExpressionCode(ao);
         }
     }
 

src/main/java/axoloti/codegen/patch/object/display/DisplayInstanceView128B.java
@@ -1,6 +1,6 @@
 package axoloti.codegen.patch.object.display;
 
-import axoloti.patch.object.display.DisplayInstanceController;
+import axoloti.patch.object.display.DisplayInstance;
 import java.nio.ByteBuffer;
 
 /**
@@ -9,8 +9,8 @@ import java.nio.ByteBuffer;
  */
 class DisplayInstanceView128B extends DisplayInstanceView {
 
-    DisplayInstanceView128B(DisplayInstanceController controller) {
-        super(controller);
+    DisplayInstanceView128B(DisplayInstance displayInstance) {
+        super(displayInstance);
     }
 
     final int n = 128;
@@ -20,11 +20,11 @@ class DisplayInstanceView128B extends DisplayInstanceView {
     int[] value;
 
     @Override
-    public String GenerateCodeInit(String vprefix) {
+    public String generateCodeInit(String vprefix) {
         String s = "{\n"
                 + "   int _i;\n"
                 + "   for(_i=0;_i<" + n + ";_i++)\n"
-                + "   " + GetCName() + "[_i] = 0;\n"
+                + "   " + getCName() + "[_i] = 0;\n"
                 + "}\n";
         return s;
     }
@@ -35,12 +35,14 @@ class DisplayInstanceView128B extends DisplayInstanceView {
     }
 
     @Override
-    public void ProcessByteBuffer(ByteBuffer bb) {
+    public void processByteBuffer(ByteBuffer bb) {
         bb.get(dst);
         for (int i = 0; i < n; i++) {
             idst[i] = dst[i];
         }
-        getController().setValue(idst);
+        DisplayInstance m = model;
+        m.getController();
+        model.getController().setValue(idst);
     }
 
 }

src/main/java/axoloti/codegen/patch/object/parameter/ParameterInstanceViewFactory.java
@@ -20,32 +20,32 @@ import axoloti.patch.object.parameter.ParameterInstanceInt32;
  */
 public class ParameterInstanceViewFactory {
 
-    public static ParameterInstanceView createView(ParameterInstanceController controller) {
-        ParameterInstance model = controller.getModel();
+    public static ParameterInstanceView createView(ParameterInstance model) {
+        ParameterInstanceController controller = model.getController();
         ParameterInstanceView view;
         // order of tests is important!
         if (model instanceof ParameterInstanceBin1Momentary) {
-            view = new ParameterInstanceViewBin1Momentary(controller);
+            view = new ParameterInstanceViewBin1Momentary(model);
         } else if (model instanceof ParameterInstanceBin1) {
-            view = new ParameterInstanceViewBin1(controller);
+            view = new ParameterInstanceViewBin1(model);
         } else if (model instanceof ParameterInstanceFrac32SMapVSlider) {
-            view = new ParameterInstanceViewFrac32S(controller);
+            view = new ParameterInstanceViewFrac32S(model);
         } else if (model instanceof ParameterInstanceFrac32UMapVSlider) {
-            view = new ParameterInstanceViewFrac32U(controller);
+            view = new ParameterInstanceViewFrac32U(model);
         } else if (model instanceof ParameterInstanceFrac32UMap) {
-            view = new ParameterInstanceViewFrac32U(controller);
+            view = new ParameterInstanceViewFrac32U(model);
         } else if (model instanceof ParameterInstanceFrac32SMap) {
-            view = new ParameterInstanceViewFrac32S(controller);
+            view = new ParameterInstanceViewFrac32S(model);
         } else if (model instanceof ParameterInstanceInt32) {
-            view = new ParameterInstanceViewInt32(controller);
+            view = new ParameterInstanceViewInt32(model);
         } else if (model instanceof ParameterInstanceBin12) {
-            view = new ParameterInstanceViewBinN(controller);
+            view = new ParameterInstanceViewBinN(model);
         } else if (model instanceof ParameterInstanceBin16) {
-            view = new ParameterInstanceViewBinN(controller);
+            view = new ParameterInstanceViewBinN(model);
         } else if (model instanceof ParameterInstanceBin32) {
-            view = new ParameterInstanceViewBinN(controller);
+            view = new ParameterInstanceViewBinN(model);
         } else if (model instanceof ParameterInstance4LevelX16) {
-            view = new ParameterInstanceView4LevelX16(controller);
+            view = new ParameterInstanceView4LevelX16(model);
         } else {
             throw new Error("ParameterInstanceViewFactory class not handled: " + model.getClass());
         }

src/main/java/axoloti/object/AxoObjectFromPatch.java
@@ -17,7 +17,6 @@
  */
 package axoloti.object;
 
-import axoloti.objectlibrary.AxoObjects;
 import axoloti.abstractui.PatchView;
 import axoloti.codegen.patch.PatchViewCodegen;
 import axoloti.mvc.AbstractDocumentRoot;
@@ -26,6 +25,7 @@ import axoloti.object.display.Display;
 import axoloti.object.inlet.Inlet;
 import axoloti.object.outlet.Outlet;
 import axoloti.object.parameter.Parameter;
+import axoloti.objectlibrary.AxoObjects;
 import axoloti.patch.PatchController;
 import axoloti.patch.PatchModel;
 import axoloti.swingui.patch.PatchFrame;
@@ -55,23 +55,23 @@ public class AxoObjectFromPatch extends AxoObject {
             patchModel = serializer.read(PatchModel.class, f);
             patchModel.setFileNamePath(f.getPath());
             patchModel.setDocumentRoot(new AbstractDocumentRoot());
-            patchController = patchModel.getControllerFromModel();
+            patchController = patchModel.getController();
         } catch (Exception ex) {
             Logger.getLogger(AxoObjects.class.getName()).log(Level.SEVERE, null, ex);
         }
-        shortId = f.getName().substring(0, f.getName().lastIndexOf("."));
+        shortId = f.getName().substring(0, f.getName().lastIndexOf('.'));
         setPath(f.getAbsolutePath());
-        UpdateObject();
+        updateObject();
 
         // TODO: review, perhaps source of multiple entries of eg. fx/flanger in objectselector
-        AxoObjects.getAxoObjects().ObjectList.add(this);
+        AxoObjects.getAxoObjects().objectList.add(this);
         // strip file extension
     }
 
-    final public void UpdateObject() {
-        PatchController controller = patchModel.getControllerFromModel();
-        PatchViewCodegen codegen = new PatchViewCodegen(controller);
-        AxoObject o = codegen.GenerateAxoObj(new AxoObject());
+    final public void updateObject() {
+        PatchController controller = patchModel.getController();
+        PatchViewCodegen codegen = new PatchViewCodegen(patchModel);
+        AxoObject o = codegen.generateAxoObj(new AxoObject());
         attributes = o.getAttributes();
         depends = o.depends;
         displays = o.getDisplays();
@@ -110,9 +110,9 @@ public class AxoObjectFromPatch extends AxoObject {
     }
 
     @Override
-    public void OpenEditor() {
+    public void openEditor() {
         if (pf == null) {
-            pf = new PatchFrame(patchController, QCmdProcessor.getQCmdProcessor());
+            pf = new PatchFrame(patchModel, QCmdProcessor.getQCmdProcessor());
         }
         pf.setVisible(true);
         pf.toFront();

src/main/java/axoloti/patch/object/parameter/ParameterInstance.java
@@ -18,10 +18,8 @@
 package axoloti.patch.object.parameter;
 
 import axoloti.Modulation;
-import axoloti.mvc.AbstractController;
 import axoloti.object.AxoObjectPatcher;
 import axoloti.object.atom.AtomDefinition;
-import axoloti.object.atom.AtomDefinitionController;
 import axoloti.object.parameter.Parameter;
 import axoloti.patch.PatchModel;
 import axoloti.patch.object.AxoObjectInstance;
@@ -46,7 +44,7 @@ import org.simpleframework.xml.Root;
  * @author Johannes Taelman
  */
 @Root(name = "param")
-public abstract class ParameterInstance<T extends Parameter, DT> extends AtomInstance<T> {
+public abstract class ParameterInstance<T extends Parameter, DT> extends AtomInstance<T, ParameterInstanceController> {
 
     @Attribute
     public String name;
@@ -55,15 +53,10 @@ public abstract class ParameterInstance<T extends Parameter, DT> extends AtomIns
     @Attribute(required = false)
     private Integer MidiCC = null;
 
-    public T parameter; // TODO: make private, create ParameterInstanceFactory
+    protected T parameter;
 
-    public NativeToReal convs[];
     NativeToReal conversion;
 //    int selectedConv = 0;
-
-    // TODO: create ParameterController class
-    // replace AtomDefinitionController with ParameterController
-    AtomDefinitionController controller;
 
     public ParameterInstance() {
     }
@@ -101,11 +94,11 @@ public abstract class ParameterInstance<T extends Parameter, DT> extends AtomIns
         return l;
     }
 
-    public String GetCName() {
-        return parameter.GetCName();
+    public String getCName() {
+        return parameter.getCName();
     }
 
-    public void CopyValueFrom(ParameterInstance p) {
+    public void copyValueFrom(ParameterInstance p) {
         if (p.onParent != null) {
             setOnParent(p.onParent);
         }
@@ -143,7 +136,7 @@ public abstract class ParameterInstance<T extends Parameter, DT> extends AtomIns
 //        return getValue().getRaw();
 //    }
 
-    public String ControlOnParentName() {
+    public String getControlOnParentName() {
         if (getParent().getParameterInstances().size() == 1) {
             return getParent().getInstanceName();
         } else {
@@ -151,13 +144,12 @@ public abstract class ParameterInstance<T extends Parameter, DT> extends AtomIns
         }
     }
 
-
-    public String GetPFunction() {
-        return "0";
+    public String getPFunction() {
+        return getDModel().getPFunction();
     }
 
     // review!
-    public String GetUserParameterName() {
+    public String getUserParameterName() {
         if (getParent().getParameterInstances().size() == 1) {
             return getParent().getInstanceName();
         } else {
@@ -169,21 +161,21 @@ public abstract class ParameterInstance<T extends Parameter, DT> extends AtomIns
 
     public abstract DT int32ToVal(int v);
 
-    public String GetCMultiplier() {
+    public String getCMultiplier() {
         return "0";
     }
 
-    public String GetCOffset() {
+    public String getCOffset() {
         return "0";
     }
 
     public String getLegalName() {
-        return CharEscape.CharEscape(getModel().getName());
+        return CharEscape.charEscape(getDModel().getName());
     }
 
     public T createParameterForParent() {
         T pcopy = (T) parameter.getClone();
-        pcopy.setName(ControlOnParentName());
+        pcopy.setName(getControlOnParentName());
         pcopy.noLabel = null;
         pcopy.PropagateToChild = getParent().getLegalName() + "_" + getLegalName();
         return pcopy;
@@ -195,11 +187,12 @@ public abstract class ParameterInstance<T extends Parameter, DT> extends AtomIns
     }
 
     @Override
-    public T getModel() {
-        return (T) getController().getModel();
+    public T getDModel() {
+        return parameter;
     }
 
-    public String GenerateCodeInitModulator(String vprefix, String StructAccces) {
+    public String generateCodeInitModulator(String vprefix, String StructAccces) {
+        // TODO: fix modulations
         return "";
     }
 
@@ -207,13 +200,14 @@ public abstract class ParameterInstance<T extends Parameter, DT> extends AtomIns
         return null;
     }
 
-    public NativeToReal[] getConvs() {
-        return convs;
+    public List<NativeToReal> getConvs() {
+        return getDModel().getConversions();
     }
 
     public NativeToReal getConversion() {
-        if ((conversion == null) && (convs != null) && (convs.length != 0)) {
-            conversion = convs[0];
+        List<NativeToReal> convs = getConvs();
+        if ((conversion == null) && (convs != null) && (!convs.isEmpty())) {
+            conversion = convs.get(0);
         }
         return conversion;
     }
@@ -224,28 +218,20 @@ public abstract class ParameterInstance<T extends Parameter, DT> extends AtomIns
     }
 
     public void cycleConversions() {
-        if ((convs == null) || (convs.length == 0)) {
+        List<NativeToReal> convs = getConvs();
+        if ((convs == null) || (convs.isEmpty())) {
             return;
         }
         if (conversion == null) {
-            setConversion(convs[0]);
+            setConversion(convs.get(0));
         }
         List l = java.util.Arrays.asList(convs);
         int i = l.indexOf(conversion);
         i++;
-        if (i >= convs.length) {
+        if (i >= convs.size()) {
             i = 0;
         }
-        setConversion(convs[i]);
-    }
-
-    @Override
-    public AtomDefinitionController getController() {
-        return controller;
-    }
-
-    public void setController(AtomDefinitionController controller) {
-        this.controller = controller;
+        setConversion(convs.get(i));
     }
 
     /* MVC getters and setters */
@@ -297,7 +283,7 @@ public abstract class ParameterInstance<T extends Parameter, DT> extends AtomIns
 
     public void updateParamOnParent() {
         if (paramOnParent != null) {
-            paramOnParent.setName(ControlOnParentName());
+            paramOnParent.setName(getControlOnParentName());
             paramOnParent.noLabel = null;
             paramOnParent.PropagateToChild = getParent().getLegalName() + "_" + getLegalName();
         }
@@ -325,12 +311,12 @@ public abstract class ParameterInstance<T extends Parameter, DT> extends AtomIns
         if (pm != null) {
             AxoObjectInstancePatcher aoip = pm.getParent();
             if (aoip != null) {
-                AxoObjectPatcher aop = (AxoObjectPatcher) aoip.getController().getModel();
+                AxoObjectPatcher aop = (AxoObjectPatcher) aoip.getDModel();
                 if (onParent != null && onParent) {
                     paramOnParent.setParent(aop);
-                    aop.getControllerFromModel().addParameter(paramOnParent);
+                    aop.getController().addParameter(paramOnParent);
                 } else {
-                    aop.getControllerFromModel().removeParameter(paramOnParent);
+                    aop.getController().removeParameter(paramOnParent);
                 }
             }
         }
@@ -364,17 +350,17 @@ public abstract class ParameterInstance<T extends Parameter, DT> extends AtomIns
     @Override
     public void dispose() {
         super.dispose();
-        getControllerFromModel().changeOnParent(false);
+        getController().changeOnParent(false);
     }
 
     @Override
-    public AbstractController createController() {
+    public ParameterInstanceController createController() {
         return new ParameterInstanceController(this);
     }
 
     @Override
-    public ParameterInstanceController getControllerFromModel() {
-        return (ParameterInstanceController) super.getControllerFromModel();
+    public ParameterInstanceController getController() {
+        return (ParameterInstanceController) super.getController();
     }
 
 }

commit 506a061bafb2965688c7288570016d97f489d184

GUI: major cleanup addendum
* fix issues introduced in commit 1ab460c:
* fix ParameterInstanceView4LexelX16
* fix parameter conversion labels
* fix npe when dragging on ionlets in objectselector

* implement nicolas' comments on commit 1ab460c

src/main/java/axoloti/codegen/patch/object/AxoObjectInstanceCodegenViewFactory.java
@@ -5,7 +5,6 @@ import axoloti.patch.object.AxoObjectInstanceComment;
 import axoloti.patch.object.AxoObjectInstanceHyperlink;
 import axoloti.patch.object.AxoObjectInstancePatcher;
 import axoloti.patch.object.IAxoObjectInstance;
-import axoloti.patch.object.ObjectInstanceController;
 
 /**
  *
@@ -14,10 +13,9 @@ import axoloti.patch.object.ObjectInstanceController;
 public class AxoObjectInstanceCodegenViewFactory {
 
     public static IAxoObjectInstanceCodegenView createView(IAxoObjectInstance model) {
-        ObjectInstanceController controller = model.getController();
         IAxoObjectInstanceCodegenView view;
         if (model instanceof AxoObjectInstancePatcher) {
-            view = new AxoObjectInstancePatcherCodegenView(model);
+            view = new AxoObjectInstancePatcherCodegenView((AxoObjectInstancePatcher) model);
         } else if (model instanceof AxoObjectInstance) {
             view = new AxoObjectInstanceCodegenView(model);
         } else if (model instanceof AxoObjectInstanceComment) {
@@ -27,7 +25,7 @@ public class AxoObjectInstanceCodegenViewFactory {
         } else {
             return null;
         }
-        controller.addView(view);
+        model.getController().addView(view);
         return view;
     }
 }

src/main/java/axoloti/codegen/patch/object/AxoObjectInstancePatcherCodegenView.java
@@ -5,7 +5,6 @@ import axoloti.object.AxoObjectPatcher;
 import axoloti.patch.PatchModel;
 import axoloti.patch.SubPatchMode;
 import axoloti.patch.object.AxoObjectInstancePatcher;
-import axoloti.patch.object.IAxoObjectInstance;
 
 /**
  *
@@ -13,11 +12,10 @@ import axoloti.patch.object.IAxoObjectInstance;
  */
 public class AxoObjectInstancePatcherCodegenView extends AxoObjectInstanceCodegenView {
 
-    AxoObjectInstancePatcherCodegenView(IAxoObjectInstance objectInstance) {
+    AxoObjectInstancePatcherCodegenView(AxoObjectInstancePatcher objectInstance) {
         super(objectInstance);
-        AxoObjectInstancePatcher model = (AxoObjectInstancePatcher) objectInstance;
-        AxoObjectPatcher ao = (AxoObjectPatcher) model.getDModel();
-        PatchModel pm = model.getSubPatchModel();
+        AxoObjectPatcher ao = (AxoObjectPatcher) objectInstance.getDModel();
+        PatchModel pm = objectInstance.getSubPatchModel();
         PatchViewCodegen pvcg = new PatchViewCodegen(pm);
 
         // from GenerateAxoObjNormal

src/main/java/axoloti/codegen/patch/object/display/DisplayInstanceView128B.java
@@ -40,8 +40,6 @@ class DisplayInstanceView128B extends DisplayInstanceView {
         for (int i = 0; i < n; i++) {
             idst[i] = dst[i];
         }
-        DisplayInstance m = model;
-        m.getController();
         model.getController().setValue(idst);
     }
 

src/main/java/axoloti/codegen/patch/object/parameter/ParameterInstanceViewFactory.java
@@ -7,7 +7,6 @@ import axoloti.patch.object.parameter.ParameterInstanceBin12;
 import axoloti.patch.object.parameter.ParameterInstanceBin16;
 import axoloti.patch.object.parameter.ParameterInstanceBin1Momentary;
 import axoloti.patch.object.parameter.ParameterInstanceBin32;
-import axoloti.patch.object.parameter.ParameterInstanceController;
 import axoloti.patch.object.parameter.ParameterInstanceFrac32SMap;
 import axoloti.patch.object.parameter.ParameterInstanceFrac32SMapVSlider;
 import axoloti.patch.object.parameter.ParameterInstanceFrac32UMap;
@@ -21,7 +20,6 @@ import axoloti.patch.object.parameter.ParameterInstanceInt32;
 public class ParameterInstanceViewFactory {
 
     public static ParameterInstanceView createView(ParameterInstance model) {
-        ParameterInstanceController controller = model.getController();
         ParameterInstanceView view;
         // order of tests is important!
         if (model instanceof ParameterInstanceBin1Momentary) {
@@ -49,7 +47,7 @@ public class ParameterInstanceViewFactory {
         } else {
             throw new Error("ParameterInstanceViewFactory class not handled: " + model.getClass());
         }
-        controller.addView(view);
+        model.getController().addView(view);
         return view;
     }
 }

src/main/java/axoloti/object/AxoObjectFromPatch.java
@@ -69,7 +69,6 @@ public class AxoObjectFromPatch extends AxoObject {
     }
 
     final public void updateObject() {
-        PatchController controller = patchModel.getController();
         PatchViewCodegen codegen = new PatchViewCodegen(patchModel);
         AxoObject o = codegen.generateAxoObj(new AxoObject());
         attributes = o.getAttributes();

src/main/java/axoloti/patch/object/parameter/ParameterInstance.java
@@ -225,8 +225,13 @@ public abstract class ParameterInstance<T extends Parameter, DT> extends AtomIns
         if (conversion == null) {
             setConversion(convs.get(0));
         }
-        List l = java.util.Arrays.asList(convs);
-        int i = l.indexOf(conversion);
+        int i = -1;
+        for (int j = 0; j < convs.size(); j++) {
+            if (convs.get(j) == conversion) {
+                i = j;
+                break;
+            }
+        }
         i++;
         if (i >= convs.size()) {
             i = 0;

src/main/java/axoloti/swingui/patch/object/AxoObjectInstanceViewAbstract.java
@@ -180,7 +180,8 @@ public class AxoObjectInstanceViewAbstract extends ViewPanel<IAxoObjectInstance>
 
     @Override
     public void mouseDragged(MouseEvent me) {
-        if (getPatchModel() == null) {
+        if (patchView == null) {
+            // we're in the ObjectSectorFrame
             return;
         }
         Point locInPatch = localToPatchLocation(me.getPoint(), me.getComponent());

src/main/java/axoloti/swingui/patch/object/parameter/ParameterInstanceView.java
@@ -68,7 +68,7 @@ public abstract class ParameterInstanceView extends ViewPanel<ParameterInstance>
         label.setText(getDModel().getDModel().getName());
         lbls.add(label);
 
-        if (getDModel().getConvs() != null) {
+        if (!getDModel().getConvs().isEmpty()) {
             lbls.add(valuelbl);
             Dimension d = new Dimension(50, 10);
             valuelbl.setMinimumSize(d);

src/main/java/axoloti/swingui/patch/object/parameter/ParameterInstanceView4LevelX16.java
@@ -2,12 +2,33 @@ package axoloti.swingui.patch.object.parameter;
 
 import axoloti.abstractui.IAxoObjectInstanceView;
 import axoloti.patch.object.parameter.ParameterInstance;
+import axoloti.patch.object.parameter.ParameterInstance4LevelX16;
+import axoloti.patch.object.parameter.preset.PresetInt;
 import axoloti.swingui.components.control.Checkbox4StatesComponent;
 
-class ParameterInstanceView4LevelX16 extends ParameterInstanceViewInt32 {
+class ParameterInstanceView4LevelX16 extends ParameterInstanceView {
 
     public ParameterInstanceView4LevelX16(ParameterInstance parameterInstance, IAxoObjectInstanceView axoObjectInstanceView) {
         super(parameterInstance, axoObjectInstanceView);
+    }
+
+    @Override
+    public ParameterInstance4LevelX16 getDModel() {
+        return (ParameterInstance4LevelX16) super.getDModel();
+    }
+
+    @Override
+    public boolean handleAdjustment() {
+        PresetInt p = null; // getDModel().getPreset(presetEditActive); // TODO: fix preset editing logic
+        if (p != null) {
+            p.setValue((int) getControlComponent().getValue());
+        } else if (getDModel().getValue() != (int) getControlComponent().getValue()) {
+            int v = (int) getControlComponent().getValue();
+            getDModel().getController().changeValue(v);
+        } else {
+            return false;
+        }
+        return true;
     }
 
     @Override

src/main/java/axoloti/swingui/patch/object/parameter/ParameterInstanceViewFrac32.java
@@ -6,6 +6,7 @@ import axoloti.datatypes.ValueFrac32;
 import axoloti.patch.object.parameter.ParameterInstance;
 import axoloti.patch.object.parameter.ParameterInstanceFrac32;
 import axoloti.patch.object.parameter.preset.PresetDouble;
+import axoloti.realunits.NativeToReal;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -39,9 +40,11 @@ abstract class ParameterInstanceViewFrac32 extends ParameterInstanceView {
     @Override
     void updateUnit() {
         super.updateUnit();
-        if (getDModel().getConversion() != null) {
-            valuelbl.setText(getDModel().getConversion().convertToReal(new ValueFrac32(
-                    getDModel().getValue())));
+        NativeToReal conv = getDModel().getConversion();
+        if (conv != null) {
+            String n = conv.convertToReal(new ValueFrac32(
+                    getDModel().getValue()));
+            valuelbl.setText(n);
         }
     }
 