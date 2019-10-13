f9b54bf335095121e9d3960ed6cf6b8e219f895e
Johannes Taelman <johannes.taelman@gmail.com>
2019-10-13 20:04:39 +0200

    win bundling: add WiX path

--- platform_win/build_release.bat
+++ platform_win/build_release.bat
@@ -3,7 +3,7 @@ setlocal
 call find_jdk || exit /b 1
 call find_ant || exit /b 1
 cd %~dp0\..
-set PATH=%PATH%;C:\msys32\usr\bin
+set PATH=%PATH%;C:\Program Files (x86)\WiX Toolset v3.11\bin;C:\msys32\usr\bin
 
 call %ANT% clean || exit /b 1
 call %ANT% || exit /b 1
