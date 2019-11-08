d3c5f18847943ebbefb708eb605a8eba79309572
Johannes Taelman <johannes.taelman@gmail.com>
2019-11-08 11:08:32 +0100

    FW: add SRAM2 heap

--- firmware/axoloti_memory.c
+++ firmware/axoloti_memory.c
@@ -3,18 +3,21 @@
 #include "sdram.h"
 #include "axoloti_memory.h"
 
-static char sram1_heap_data[60*1024]  __attribute__ ((section (".ram0")));
-static char ccm_heap_data[48*1024]  CCM;
+static char sram1_heap_data[60*1024] __attribute__ ((section (".ram0")));
+static char sram2_heap_data[8*1024] __attribute__ ((section (".ram2")));;
 static char sram3_heap_data[64*1024] SRAM3;
+static char ccm_heap_data[48*1024] CCM;
 
 static memory_heap_t sdram_heap;
 static memory_heap_t sram1_heap;
+static memory_heap_t sram2_heap;
 static memory_heap_t sram3_heap;
 static memory_heap_t ccm_heap;
 
 void axoloti_mem_init(void) {
 //  chHeapObjectInit(&sram1_heap, (void *)(0x0FFFFFFF /*aliased*/ & (int)sram1_heap_data), sizeof(sram1_heap_data));
   chHeapObjectInit(&sram1_heap, (void *)sram1_heap_data, sizeof(sram1_heap_data));
+  chHeapObjectInit(&sram2_heap, (void *)sram2_heap_data, sizeof(sram2_heap_data));
   chHeapObjectInit(&sram3_heap, (void *)sram3_heap_data, sizeof(sram3_heap_data));
   chHeapObjectInit(&sdram_heap, (void *)SDRAM_BANK_ADDR, 8*1024*1024);
   chHeapObjectInit(&ccm_heap, (void *)ccm_heap_data, sizeof(ccm_heap_data));
@@ -24,6 +27,12 @@ int sram1_available(void) {
   size_t sram1_available, sram1_largestp;
   chHeapStatus(&sram1_heap, &sram1_available, &sram1_largestp);
   return sram1_available;
+}
+
+int sram2_available(void) {
+  size_t sram3_available, sram3_largestp;
+  chHeapStatus(&sram3_heap, &sram3_available, &sram3_largestp);
+  return sram3_available;
 }
 
 int sram3_available(void) {
@@ -55,9 +64,9 @@ void* ax_malloc_align(size_t size, mem_type_flags_t mem_type, unsigned align) {
   if (mem_type & mem_type_hint_large) {
     return chHeapAllocAligned(&sdram_heap, size, align);
   }
-//  if (mem_type & mem_type_hint_tiny_dma) {
-//    return chHeapAllocAligned(&sram2_heap, size, align);
-//  }
+  if (mem_type & mem_type_hint_tiny_dma) {
+    return chHeapAllocAligned(&sram2_heap, size, align);
+  }
   if (mem_type & mem_type_hint_no_dma) {
     return chHeapAllocAligned(&ccm_heap, size, align);
   }
