4cb1a2d6f4cbfa9b3b793fa9bbc811255e56b242
Johannes Taelman <johannes.taelman@gmail.com>
2019-12-03 23:18:53 +0100

    FW: export sp float math functions

--- firmware/exports/axoloti_math.h
+++ firmware/exports/axoloti_math.h
@@ -5,6 +5,28 @@
 
 #define EXPORTS_AXOLOTI_MATH_SYMBOLS \
     SYM(pitcht), \
-    SYM(sine2t)
+    SYM(sine2t), \
+    SYM(atanf), \
+    SYM(cosf), \
+    SYM(sinf), \
+    SYM(tanf), \
+    SYM(tanhf), \
+    SYM(frexpf), \
+    SYM(modff), \
+    SYM(ceilf), \
+    SYM(fabsf), \
+    SYM(floorf), \
+    SYM(acosf), \
+    SYM(asinf), \
+    SYM(atan2f), \
+    SYM(coshf), \
+    SYM(sinhf), \
+    SYM(expf), \
+    SYM(ldexpf), \
+    SYM(logf), \
+    SYM(log10f), \
+    SYM(powf), \
+    SYM(sqrtf), \
+    SYM(fmodf)
 
 #endif
